bceeb9c425c6a259f30ced8b37189268ffef434d
danstalcup <billyjoelntrdame@gmail.com>
2017-08-14 21:28:09 -0400

    Console program.
    Console program. Fix CSV filename typo.

--- MarkovWord/Program.cs
+++ MarkovWord/Program.cs
@@ -10,6 +10,27 @@ namespace MarkovWord
     {
         static void Main(string[] args)
         {
+            string filename = "letterprobabilities.csv";
+            int seed = WordGenerator.NoSeed;
+            int numWords = 10;
+
+            if (args.Length >= 1)
+            {
+                numWords = int.Parse(args[0]);
+            }
+            if (args.Length >= 2)
+            {
+                seed = int.Parse(args[1]);
+            }
+            if (args.Length >= 3)
+            {
+                filename = args[2];
+            }
+
+            var engine = new WordGenerator(filename);
+            engine.Initialize(seed);
+
+            Console.WriteLine(engine.GetWords(numWords));
 
         }
     }
